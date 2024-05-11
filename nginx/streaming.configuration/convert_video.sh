6b4cf00ca9ccd079e7dccef3da35f504f61ae213
frederic.torres@bigtincan.com <frederic.torres@bigtincan.com>
2024-05-11 16:10:17 -0400

    Update...

--- nginx/streaming.configuration/convert_video.sh
+++ nginx/streaming.configuration/convert_video.sh
@@ -20,12 +20,12 @@ ToHls() {
 ToHls1080p() {
     local filename=$1
     local idname=$2
-    local medium = "medium"
+    local medium= "medium"
 # Resolution #0 supplied is 1920x1080 1080p 5Mb/s
 # Resolution #1 960x540 1080p/2 3 Mb/s
 
     sudo ffmpeg -i "$filename" \
-    -filter_complex "[0:v]split=3[v1][v2]; [v1]copy[v1out]; [v2]scale=w=960:h=540[v2out];" \
+    -filter_complex "[0:v]split=2[v1][v2]; [v1]copy[v1out]; [v2]scale=w=960:h=540[v2out]" \
     -map "[v1out]" -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 5M  -preset "$medium" -g 48 -sc_threshold 0 -keyint_min 48 \
     -map "[v2out]" -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M  -preset "$medium" -g 48 -sc_threshold 0 -keyint_min 48 \
     -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 \
@@ -35,6 +35,22 @@ ToHls1080p() {
     -hls_segment_filename "$idname-%v/data%02d.ts" \
     -master_pl_name "master.m3u8" \
     -var_stream_map "v:0,a:0 v:1,a:1" "$idname-%v.m3u8"     
+
+# sudo ffmpeg -i "$filename" 
+# -filter_complex "[0:v]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=640:h=360[v3out]" 
+# -map "[v1out]" -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 5M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 
+# -map "[v2out]" -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 
+# -map "[v3out]" -c:v:2 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 
+
+# -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 
+# -map a:0 -c:a:1 aac -b:a:1 96k -ac 2 
+# -map a:0 -c:a:2 aac -b:a:2 48k -ac 2 
+# -f hls -hls_time 2 -hls_playlist_type vod -hls_flags independent_segments 
+# -hls_segment_type mpegts -hls_segment_filename "$idname-%v/data%02d.ts" 
+# -master_pl_name "master.m3u8" -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" 
+# "$idname-%v.m3u8"
+
+
 }
 
 pause() {
