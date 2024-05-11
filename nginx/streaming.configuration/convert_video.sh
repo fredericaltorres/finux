e18dd294b9fd4db1cb760f8d91b6945465f05bc5
frederic.torres@bigtincan.com <frederic.torres@bigtincan.com>
2024-05-11 15:54:00 -0400

    Update...

--- nginx/streaming.configuration/convert_video.sh
+++ nginx/streaming.configuration/convert_video.sh
@@ -17,6 +17,24 @@ ToHls() {
     echo "video streaming url http://[IP]:8088/hls/$idname/$idname-0.m3u8"
 }
 
+ToHls1080p() {
+
+    local medium = "medium"
+# Resolution #0 supplied is 1920x1080 1080p 5Mb/s
+# Resolution #1 960x540 1080p/2 3 Mb/s
+
+    sudo ffmpeg -i "$filename" \
+    -filter_complex "[0:v]split=3[v1][v2]; [v1]copy[v1out]; [v2]scale=w=960:h=540[v2out];" \
+    -map "[v1out]" -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 5M  -preset "$medium" -g 48 -sc_threshold 0 -keyint_min 48 \
+    -map "[v2out]" -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M  -preset "$medium" -g 48 -sc_threshold 0 -keyint_min 48 \
+    -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 \
+    -map a:0 -c:a:1 aac -b:a:1 96k -ac 2 \
+    -f hls -hls_time 2 -hls_playlist_type vod \
+    -hls_flags independent_segments -hls_segment_type mpegts \
+    -hls_segment_filename "$idname-%v/data%02d.ts" \
+    -master_pl_name "master.m3u8" \
+    -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" "$idname-%v.m3u8" \    
+}
 
 pause() {
     echo "Press Enter to continue..."; read;
@@ -42,21 +60,21 @@ cd /home/videos
 echo "Convert to hls fredffmpeg video conversion in multiple resolution"
 ## ffmpeg -i "/home/videos/fredband.mp4" -filter_complex "[0:v]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=640:h=360[v3out]" -map "[v1out]" -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 10M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 -map "[v2out]" -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 -map "[v3out]" -c:v:2 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 -map a:0 -c:a:1 aac -b:a:1 96k -ac 2 -map a:0 -c:a:2 aac -b:a:2 48k -ac 2 -f hls -hls_time 2 -hls_playlist_type vod -hls_flags independent_segments -hls_segment_type mpegts -hls_segment_filename fredband2_%v/data%02d.ts -master_pl_name master.m3u8 -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" fredband2_%v.m3u8
 # ToHls "/home/videos/fredband.mp4" "fredbandband" # use a local file
-ToHls "$AndYourBirdCanSing_video_url" "AndYourBirdCanSing" # use a url
+ToHls1080p "$AndYourBirdCanSing_video_url" "AndYourBirdCanSing" # use a url
 pause
 
 ToHls "$fredband_video_url" "fredbandband" # use a url
 pause
 
-curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/master.m3u8
-curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/fredbandband-0.m3u8
-curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/fredbandband-0.m3u8
+# curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/master.m3u8
+# curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/fredbandband-0.m3u8
+# curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/fredbandband-0.m3u8
 
-# lb 20.33.73.31 
-http://74.249.130.23:8088/hls/fredbandband/master.m3u8
-http://74.249.130.23:8088/hls/fredbandband/fredbandband-0.m3u8
+# # lb 20.33.73.31 
+# http://74.249.130.23:8088/hls/fredbandband/master.m3u8
+# http://74.249.130.23:8088/hls/fredbandband/fredbandband-0.m3u8
 
 
-curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/master.m3u8
+# curl.exe https://faiwebapiapimanagementservices.azure-api.net/video/hls/fredbandband/master.m3u8
 
 
